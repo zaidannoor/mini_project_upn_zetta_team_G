ac24d48a9222d23b904cbb2182dc58f6413edc68
Zaidan Noor Irfan <zaidannoor06@gmail.com>
2022-11-28 15:37:50 +0700

    Adding Routing to Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path :'login',
+    loadChildren:()=>import('./login/login.module').then(m=> m.LoginModule),
+  },
+  {
+    path :'cart',
+    loadChildren:()=>import('./cart/cart.module').then(m=> m.CartModule),
+  },
+  {
+    path :'home',
+    loadChildren:()=>import('./home/home.module').then(m=> m.HomeModule),
+  },
+  {path: '**', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
